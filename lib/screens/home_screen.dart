9c7fe96c1a39db663d4ecf2e2d3ed9567ec359da
telkomdev-raveendra <raveendraalkautsar@gmail.com>
2021-04-03 20:08:24 +0700

    appbar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -43,9 +43,9 @@ class _HomeScreenState extends State<HomeScreen> {
           children: <Widget>[
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Image.asset("assets/images/logo.png"),
-              ],
+                children: <Widget>[
+                  Image.asset("assets/images/logo.png"),
+                ],
             ),
             SizedBox(height: screenHeight * 0.03),
             Column(
@@ -68,51 +68,6 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ),
                 SizedBox(height: screenHeight * 0.03),
-                // Row(
-                //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                //   children: <Widget>[
-                //     FlatButton.icon(
-                //       padding: const EdgeInsets.symmetric(
-                //         vertical: 10.0,
-                //         horizontal: 20.0,
-                //       ),
-                //       onPressed: () {},
-                //       color: Colors.red,
-                //       shape: RoundedRectangleBorder(
-                //         borderRadius: BorderRadius.circular(30.0),
-                //       ),
-                //       icon: const Icon(
-                //         Icons.phone,
-                //         color: Colors.white,
-                //       ),
-                //       label: Text(
-                //         'Call Now',
-                //         style: Styles.buttonTextStyle,
-                //       ),
-                //       textColor: Colors.white,
-                //     ),
-                //     FlatButton.icon(
-                //       padding: const EdgeInsets.symmetric(
-                //         vertical: 10.0,
-                //         horizontal: 20.0,
-                //       ),
-                //       onPressed: () {},
-                //       color: Colors.blue,
-                //       shape: RoundedRectangleBorder(
-                //         borderRadius: BorderRadius.circular(30.0),
-                //       ),
-                //       icon: const Icon(
-                //         Icons.chat_bubble,
-                //         color: Colors.white,
-                //       ),
-                //       label: Text(
-                //         'Send SMS',
-                //         style: Styles.buttonTextStyle,
-                //       ),
-                //       textColor: Colors.white,
-                //     ),
-                //   ],
-                // ),
               ],
             )
           ],
